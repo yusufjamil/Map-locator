3a14f79986c46a89d78e0364ecc7c91e1c186255
yusufjamil <yusufjamil75@gmail.com>
2021-12-12 09:07:31 +0530

    places list added

--- frontend/src/Shared/Components/Navigation/MainNavigation.js
+++ frontend/src/Shared/Components/Navigation/MainNavigation.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React,{useState} from "react";
 
 import './MainNavigation.css';
-
-import MainHeader from './MainHeader';
 
 import {Link} from 'react-router-dom';
 
 import NavLinks from "./NavLinks";
- 
+
+import SideDrawer from "./SideDrawer";
+import MainHeader from "./MainHeader";
+import Backdrop from "../UIElements/Backdrop";
 
 const MainNavigation = props => {
+const[drawerIsOpen,setDrawerIsOpen]=useState(false);
+const openDrawerHandler= ()=>{
+    setDrawerIsOpen(true);
+};
+const closeDrawerHandler=()=>
+{
+    setDrawerIsOpen(false);
+}
     return ( 
+    <React.Fragment>
+    {drawerIsOpen && <Backdrop onClick={closeDrawerHandler}/> }
+    <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
+        <nav className="main-navigation__drawer-nav">
+         <NavLinks/>
+        </nav>
+    </SideDrawer>
     <MainHeader>
-        {/* <button className="main-navigation__menu-btn">
+         <button className="main-navigation__menu-btn" onClick={openDrawerHanler}>
+             {/* <Span/>
             <Span/>
-            <Span/>
-            <Span/>
-        </button> */}
+            <Span/>  */}
+        </button> 
          <h1 className="main-navigation__title">
          <Link to="/">
           Your Places
           </Link>
 
          </h1>
-         <nav>
+         <nav className="main-navigation__header-nav">
           <NavLinks/>
          </nav>
     </MainHeader>
+    </React.Fragment>
     );
 };
 
